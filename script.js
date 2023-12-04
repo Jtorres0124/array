a3641b42d965190a896b60b765a4003cf337fc43
Jtorres0124 <143359112+Jtorres0124@users.noreply.github.com>
2023-12-04 17:43:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// Part I: Log even numbers from 1000 to 0
+
 console.log("Even numbers from 1000 to 0:");
 for (let i = 1000; i >= 0; i -= 2) {
     console.log(i);
 }
 
-// Part II: Alert messages at specific points in the loop
+
 console.log("\nAlert messages during loop execution:");
 for (let i = 0; i <= 10000; i++) {
     if (i === 2500) {
@@ -16,10 +16,10 @@ for (let i = 0; i <= 10000; i++) {
     }
 }
 
-// Part III: Array of top 5 favorite TV shows
+
 const favoriteTVShows = ["Breaking Bad", "The flash", "The Boys", "Black Mirror", "Supernatural"];
 
-// Loop through the array and log messages
+
 console.log("\nLoop through favorite TV shows:");
 for (let i = 0; i < favoriteTVShows.length; i++) {
     console.log(`My #${i + 1} favorite TV show is ${favoriteTVShows[i]}`);
